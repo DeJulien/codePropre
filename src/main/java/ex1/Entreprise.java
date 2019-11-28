09fa6f10b43d13b514d311260f78d126ed09816f
Julien Dewost <dewostjulien@gmail.com>
2019-11-28 14:14:29 +0100

    exo 1 maj 1

--- src/main/java/ex1/Entreprise.java
+++ src/main/java/ex1/Entreprise.java
@@ -9,20 +9,32 @@ import java.util.Date;
 
 public class Entreprise {
 
-	/** 
-	 * Variable entier du siret
-	 * Chaine de caractere du nom
-	 * chaine de caractere de l'adresse
-	 * Variable Date de la creation 
-	 * */
-	public int Siret;
-	public String Nom;
-	public String adresse;
-	public Date date_Creation;
+	/**  Variable entier du siret */
+	private long Siret;
+	/**Chaine de caractere du nom **/
+	private String Nom;
+	/** chaine de caractere de l'adresse **/
+	private String adresse;
+	/**Variable Date de la creation **/
+	private Date date_Creation;
 	
 	/** Nombre maximum de capital */
 	public static final int CAPITAL_MAX = 3000000;
 	
+	/** Constructor de entreprise
+	 * 
+	 * @param siret de type long
+	 * @param nom de type string
+	 * @param adresse de type string
+	 * @param date_creation de type date
+	 */
+	public Entreprise(long siret, String nom, String adresse, Date date_Creation) {
+		this.Siret = siret;
+		this.Nom = nom;
+		this.adresse = adresse;
+		this.date_Creation = date_Creation;
+	}
+
 	/** Methode pour afficher le statut
 	 * 
 	 * 
